546648c105a36b19ec168126478b43b06b1535d0
lizardman2297 <fariello.loris@gmail.com>
2021-01-26 13:39:21 +0100

    modif

--- model/formulaireMaterielAjoutModel.php
+++ model/formulaireMaterielAjoutModel.php
@@ -1,48 +1,58 @@
 <?php
 
-$target_dir = "public/images/produit";
-$target_file = $target_dir . basename($_FILES["imageProduit"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    define('TARGET', '../public/images/produit');    
+    define('MAX_SIZE', 100000000);    
+    define('WIDTH_MAX', 80000);    
+    define('HEIGHT_MAX', 80000);   
+     
+    $tabExt = array('jpg','gif','png','jpeg');    
+    $infosImg = array();
+     
+    $extension = '';
+    $message = '';
+    $nomImage = '';
+     
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["imageProduit"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+if( !is_dir(TARGET) ){
+    if( !mkdir(TARGET, 0755) ){
+      exit('Erreur : le répertoire cible ne peut-être créé ! Vérifiez que vous diposiez des droits suffisants pour le faire ou créez le manuellement !');
     }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["imageProduit"]["size"] > 50000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Allow certain file formats
-// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "webp") {
-//     echo "Sorry, only JPG, JPEG, PNG & GIF & webp files are allowed.";
-//     $uploadOk = 0;
-// }
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["imageProduit"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["imageProduit"]["name"])). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+  }
+   
+  if(!empty($_POST)){
+    if( !empty($_FILES['fichier']['name']) ){
+      $extension  = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+   
+      if(in_array(strtolower($extension),$tabExt)){
+        $infosImg = getimagesize($_FILES['fichier']['tmp_name']);
+   
+        if($infosImg[2] >= 1 && $infosImg[2] <= 14){
+          if(($infosImg[0] <= WIDTH_MAX) && ($infosImg[1] <= HEIGHT_MAX) && (filesize($_FILES['fichier']['tmp_name']) <= MAX_SIZE)){
+            if(isset($_FILES['fichier']['error']) && UPLOAD_ERR_OK === $_FILES['fichier']['error']){
+              $nomImage = md5(uniqid()) .'.'. $extension;
+              if(move_uploaded_file($_FILES['fichier']['tmp_name'], TARGET.$nomImage)){
+                    $message = 'Modification effectuer avec succes !';
+              }else{
+                  $message = 'Problème lors de l\'upload !';
+              }
+            }else{
+                  $message = 'Une erreur interne a empêché l\'uplaod de l\'image';
+            }
+          }else{
+              $message = 'Erreur dans les dimensions de l\'image !';
+          }
+        }else{
+              $message = 'Le fichier à uploader n\'est pas une image !';
+        }
+      }else{
+          $message = 'L\'extension du fichier est incorrecte !';
+      }
+    }else{
+          $message = 'Veuillez remplir le formulaire svp !';
     }
-}
+  }else {
+      var_dump($_GET);
+      $message = "vide";
+  }
+
+  echo $message;
--- view/materiel/materielAjoutView.php
+++ view/materiel/materielAjoutView.php
@@ -9,36 +9,36 @@
     <div class="container">
         Forumulaire ajout materiel
 
-        <form method="POST" action="model/formulaireMaterielAjoutModel.php">
-            <div class="form-group">
+        <form enctype="multipart/form-data" action="model/formulaireMaterielAjoutModel.php" method="GET" >
+            <!-- <div class="form-group"> -->
                 <label for="nomProduit">Nom</label>
                 <input type="text" class="form-control" id="nomProduit">
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="refProduit">Réference</label>
                 <input type="text" class="form-control" id="refProduit">
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="dateAchatProduit">Date achat</label>
                 <input type="date" class="form-control" id="dateAchatProduit">
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="marqueProduit">marque</label>
                 <input type="text" class="form-control" id="marqueProduit">
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="quantiteProduit">quantité</label>
                 <input type="text" class="form-control" id="quantiteProduit">
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="statutProduit">Statut</label>
                 <select class="form-control" id="statutProduit">
                     <option>Actuel</option>
                     <option>Liste de souhait</option>
                     <option>ancien</option>
                 </select>
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="typrProduit">Type</label>
                 <select class="form-control" id="typrProduit">
                 <?php
@@ -49,16 +49,23 @@
                 }
                 ?>
                 </select>
-            </div>
-            <div class="form-group">
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
                 <label for="commentaireProduit">Commentaire</label>
                 <textarea class="form-control" id="commentaireProduit" rows="3"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="imageProduit">Image</label>
-                <input type="file" class="form-control-file" id="imageProduit">
-            </div>
+            <!-- </div> -->
+            <!-- <div class="form-group"> -->
+                <label for="fichier_a_uploader">Image</label>
+                <input type="file" name="fichier" id="fichier_a_uploader">
+            <!-- </div> -->
             <button class="btn btn-primary">Ajouter</button>
+        </form>
+
+
+        <form enctype="multipart/form-data" action="model/formulaireMaterielAjoutModel.php" method="get">
+            <input type="text" name="test1" id="test1">
+            <input type="text" name="test2" id="test2">
+            <input type="submit" value="test">
         </form>
     </div>
 </body>
